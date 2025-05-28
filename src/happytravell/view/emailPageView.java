af72f8f02227004977b11dc8d6a09631573679ff
Your Name <youremail@example.com>
2025-05-28 16:44:42 +0545

    Traveler Dashboard frontend completed

--- src/happytravell/view/emailPageView.java
+++ src/happytravell/view/emailPageView.java
@@ -8,12 +8,12 @@ package happytravell.view;
  *
  * @author aashl
  */
-public class emailPage extends javax.swing.JFrame {
+public class emailPageView extends javax.swing.JFrame {
 
     /**
      * Creates new form emailPage
      */
-    public emailPage() {
+    public emailPageView() {
         initComponents();
     }
 
@@ -109,20 +109,21 @@ public class emailPage extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(emailPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(emailPageView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(emailPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(emailPageView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(emailPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(emailPageView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(emailPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(emailPageView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new emailPage().setVisible(true);
+                new emailPageView().setVisible(true);
             }
         });
     }
