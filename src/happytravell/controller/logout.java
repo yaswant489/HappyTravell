15c87518f8410a8ca8de450ec54cef469cbd4fd9
Administrator <khatiwadaritika243@gmail.com>
2025-05-28 19:57:54 +0545

    Map frontend completed

---
+++ src/happytravell/controller/logout.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package happytravell.controller;
+
+import javax.swing.JOptionPane;
+
+public class logout {
+
+    public void performLogout() {
+        JOptionPane.showMessageDialog(null, "You have been logged out successfully.");
+        System.exit(0); // Ends the app after logout
+    }
+}
+
