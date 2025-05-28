c0ebfc4c8cea1df13d5decbaed6a48213db5a1c5
Your Name <youremail@example.com>
2025-05-28 10:03:09 +0545

    Admin Dashboard frontend completed

---
+++ src/happytravell/controller/RegisterController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package happytravell.controller;
+
+/**
+ *
+ * @author User
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+public class RegisterController {
+    public static ArrayList<Map<String, String>> users = new ArrayList<>();
+}
+
+    
+
